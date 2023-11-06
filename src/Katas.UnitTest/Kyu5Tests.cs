2a8d9dd4d8d52d7cace3110035e8b9c8652b112c
Andrew Palmer <ahpalmer72@gmail.com>
2023-11-06 15:04:51 -0500

    Completed first Kyu5 Kata CSharp

---
+++ src/Katas.UnitTest/Kyu5Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas.UnitTest;
+
+[TestClass]
+public class Kyu5Tests
+{
+    [TestMethod]
+    public void Rot13Tests()
+    {
+        Assert.AreEqual("ROT13 example.", Kyu5.Rot13("EBG13 rknzcyr."));
+    }
+}
---
+++ src/Katas/Kyu5.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Xml.Schema;
+
+namespace Katas;
+
+internal class Kyu5
+{
+    //Time: 45 Minutes
+    internal static string Rot13(string input)
+    {
+        string pattern = @"[a-zA-Z]";
+        Regex regex = new Regex(pattern);
+        MatchEvaluator matchEval = new MatchEvaluator(ReplaceRot13Low);
+
+        string answer = regex.Replace(input, matchEval);
+
+        return answer;
+    }
+
+    public static string ReplaceRot13Low(Match match)
+    {
+        char letter = match.Value.ToString()[0];
+        int asciiInt = (int)letter;
+        if (asciiInt > 77 && asciiInt < 91)
+        {
+            char answer = System.Convert.ToChar(asciiInt - 13);
+            return answer.ToString();
+        }
+        else if (asciiInt < 78)
+        {
+            char answer = System.Convert.ToChar(asciiInt + 13);
+            return answer.ToString();
+        }
+        else if (asciiInt > 96 && asciiInt <= 109)
+        {
+            char answer = System.Convert.ToChar(asciiInt + 13);
+            return answer.ToString();
+        }
+        else if (asciiInt > 109 && asciiInt < 123)
+        {
+            char answer = System.Convert.ToChar(asciiInt - 13);
+            return answer.ToString();
+        }
+        else
+        {
+            return "Error";
+        }
+    }
+}
