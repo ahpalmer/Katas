e2e9970defa4c4988d803474a655dc3d62d2565b
Andrew Palmer <andrewpalmer@microsoft.com>
2023-11-08 16:24:03 -0500

    First Python Kata

---
+++ src/PythonKatas/PythonKatas/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,10 @@
+def number_to_string(num):
+    string_number = str(num)
+    return string_number
+
+def main():
+    string1 = number_to_string(123)
+    print(string1)
+
+if __name__ == "__main__":
+    main()
