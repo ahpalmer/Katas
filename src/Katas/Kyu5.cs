aa36d8532d34e587a59edf599ff5910f098943c2
Andrew Palmer <ahpalmer72@gmail.com>
2023-11-30 10:33:40 -0500

    Added a RGB example from codewars

--- src/Katas/Kyu5.cs
+++ src/Katas/Kyu5.cs
@@ -39,6 +39,14 @@ internal class Kyu5
 
         return String.Join("", strings);
     }
+    //Here's a better example from codewars:
+    public static string RgbExample(int r, int g, int b)
+    {
+        r = Math.Max(Math.Min(255, r), 0);
+        g = Math.Max(Math.Min(255, g), 0);
+        b = Math.Max(Math.Min(255, b), 0);
+        return String.Format("{0:X2}{1:X2}{2:X2}", r, g, b);
+    }
 
     internal static string ToUnderscore(string str)
     {
