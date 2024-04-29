8193b6722fb8cc22719479f6500a4545b97589d1
Andrew Palmer <ahpalmer72@gmail.com>
2024-04-29 11:36:29 -0400

    Kyu5 powershell.  Used Modules instead of function.  Huge pain to debug and run when compared to an individual function.  Significant bug in the code that prevents this from working, not going to fix.

--- src/PowershellKatas/Get-Fibonnaci/Get-Fibonnaci.psm1
+++ src/PowershellKatas/Get-Fibonnaci/Get-Fibonnaci.psm1
@@ -5,21 +5,39 @@
 function Get-Fibonnaci($prod) {
     $tempFibList = @()
     $boolAnswer = $false
-
+    $loopBool = $false
     $tempFibList = Get-FibonnaciFirstTen
+    $counter = 0
+    $lastFibPreviousLoop = 0
 
-    if (($tempFibList[8] * $tempFibList[9]) -ge $prod) {
-        $prodList = Get-FibonnaciProduct -fibonnaciList $tempFibList
-        $prodAnswer = $prodList | Where-Object {$_ -ge $prod}
+    while (-not $loopBool) {
+        if (($tempFibList[8] * $tempFibList[9]) -ge $prod) {
+            $prodList = Get-FibonnaciProduct -fibonnaciList $tempFibList
+            $prodAnswer = $prodList | Where-Object {$_ -ge $prod}
 
-        if ($prodAnswer -eq $prod) {
-            $boolAnswer = $true
+            if ($prodAnswer -eq $prod) {
+                $boolAnswer = $true
+            }
+
+            $loopBool = $true
+        }
+
+        else {
+            $lastFibPreviousLoop = $tempFibList[9]
+            $counter += 10
+            $tempFibList = Get-FibonnaciBatch -n $counter
         }
     }
 
+
     $index = $prodList.IndexOf($prodAnswer)
     $answerOne = $tempFibList[$index]
-    $answerTwo = $tempFibList[$index + 1]
+    if($index -eq -1) {
+        $answerTwo = $lastFibPreviousLoop
+    }
+    else {
+        $answerTwo = $tempFibList[$index + 1]
+    }
 
     return ($answerOne, $answerTwo, $boolAnswer) 
 }
