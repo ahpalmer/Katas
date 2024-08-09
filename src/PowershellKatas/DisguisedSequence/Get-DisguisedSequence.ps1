5503bb799861fe548e35f7ec60242fd8f9b96bbd
Andrew Palmer <ahpalmer72@gmail.com>
2024-08-09 15:48:25 -0400

    Completed Kyu6 powershell

--- src/PowershellKatas/DisguisedSequence/Get-DisguisedSequence.ps1
+++ src/PowershellKatas/DisguisedSequence/Get-DisguisedSequence.ps1
@@ -1,9 +1,18 @@
 # return a long
 function Get-DisguisedSequence($n) {
-    
+    $Un = 1
+    $Unplus1 = 2
+    for ($i = 1; $i -le $n; $i++)
+    {
+        $UnTemp = $Unplus1
+        $Unplus1 = Invoke-DisSeqCalculation $Un $Unplus1
+        $Un = $UnTemp
+    }
+    return $Un
 }
 
 function Invoke-DisSeqCalculation($Un, $Unplus1) {
     $answer = 6 * ($Un * $Unplus1) / ((5*$Un) - $Unplus1)
     return $answer
 }
+
